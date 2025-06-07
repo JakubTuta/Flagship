47c66d0cd581348326f213572a5ba84af2aba590
JakubTuta <jakubtutka02@gmail.com>
2025-06-07 14:07:45 +0200

    fix hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -88,7 +88,7 @@ export default defineNuxtConfig({
   experimental: {
     payloadExtraction: false,
     renderJsonPayloads: false,
-    appManifest: false,
+    appManifest: true,
   },
 
   typescript: {
