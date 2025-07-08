d55a9f5d91b385c1165c38c25c73096b37bb85a0
JakubTuta <jakubtutka02@gmail.com>
2025-07-09 01:46:12 +0200

    try fix cookies

--- stores/language.ts
+++ stores/language.ts
@@ -2,14 +2,11 @@ export const useLanguageStore = defineStore('language', () => {
   const { locale } = useI18n()
   type Language = 'en' | 'pl'
 
-  const langCookie = useCookie<Language>('tuta-lang')
+  const currentLang = computed(() => locale.value as Language)
 
   const setLanguage = (lang: Language) => {
     locale.value = lang
-    langCookie.value = lang
   }
-
-  const currentLang = computed(() => locale.value as Language)
 
   const toggleLanguage = () => {
     const newLang = locale.value === 'pl'
@@ -17,14 +14,6 @@ export const useLanguageStore = defineStore('language', () => {
       : 'pl'
     setLanguage(newLang)
   }
-
-  onMounted(() => {
-    setTimeout(() => {
-      if (langCookie.value) {
-        setLanguage(langCookie.value)
-      }
-    }, 0)
-  })
 
   return {
     currentLang,
