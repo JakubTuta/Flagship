e9af56901b1d147e67c1e72991b9ed199853a8be
JakubTuta <jakubtutka02@gmail.com>
2025-07-09 01:30:07 +0200

    try fix cookies

--- stores/language.ts
+++ stores/language.ts
@@ -2,22 +2,28 @@ export const useLanguageStore = defineStore('language', () => {
   const { locale } = useI18n()
   type Language = 'en' | 'pl'
 
-  const currentLang = computed({
-    get: () => locale.value as Language,
-    set: (lang: Language) => {
-      locale.value = lang
-    },
-  })
+  const langCookie = useCookie<Language>('tuta-lang')
 
   const setLanguage = (lang: Language) => {
-    currentLang.value = lang
+    locale.value = lang
+    langCookie.value = lang
   }
 
+  const currentLang = computed(() => locale.value as Language)
+
   const toggleLanguage = () => {
-    currentLang.value = currentLang.value === 'pl'
-      ? 'en'
-      : 'pl'
+    const newLang = locale.value === 'pl' ? 'en' : 'pl'
+    setLanguage(newLang)
   }
+
+  // This hook runs only on the client, after the app has mounted.
+  onMounted(() => {
+    // We let the server render with the default, then apply the user's
+    // preference on the client.
+    if (langCookie.value) {
+      setLanguage(langCookie.value)
+    }
+  })
 
   return {
     currentLang,
--- stores/theme.ts
+++ stores/theme.ts
@@ -1,18 +1,32 @@
 import { useTheme } from 'vuetify'
 
 export const useThemeStore = defineStore('theme', () => {
-  const colorMode = useColorMode()
   const vuetifyTheme = useTheme()
+  const themeCookie = useCookie('tuta-theme')
+
+  const setTheme = (newTheme: string) => {
+    vuetifyTheme.global.name.value = newTheme
+    themeCookie.value = newTheme
+  }
 
   const toggleTheme = () => {
-    colorMode.preference = colorMode.value === 'dark'
-      ? 'light'
-      : 'dark'
+    const newTheme = vuetifyTheme.global.name.value === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
   }
 
   const isDark = computed(() => vuetifyTheme.global.name.value === 'dark')
 
+  // This hook runs only on the client, after the app has mounted.
+  onMounted(() => {
+    // We let the server render with the default, then apply the user's
+    // preference on the client.
+    if (themeCookie.value) {
+      setTheme(themeCookie.value)
+    }
+  })
+
   return {
+    setTheme, // Exposing setTheme for direct use if needed
     toggleTheme,
     isDark,
   }
