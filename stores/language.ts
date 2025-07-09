cf5ae739a745e680a498007d6589c6db348994d8
JakubTuta <jakubtutka02@gmail.com>
2025-07-09 02:05:39 +0200

    try fix cookies

--- stores/language.ts
+++ stores/language.ts
@@ -1,12 +1,24 @@
 export const useLanguageStore = defineStore('language', () => {
   const { locale } = useI18n()
   type Language = 'en' | 'pl'
-
-  const currentLang = computed(() => locale.value as Language)
+  console.log('useLanguageStore')
+  // eslint-disable-next-line node/prefer-global/process
+  console.log(process.env.NODE_ENV)
+  const langCookie = useCookie<Language>('tuta-lang', {
+    default: () => 'en',
+    // eslint-disable-next-line node/prefer-global/process
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    httpOnly: false,
+    maxAge: 60 * 60 * 24 * 365,
+  })
 
   const setLanguage = (lang: Language) => {
     locale.value = lang
+    langCookie.value = lang
   }
+
+  const currentLang = computed(() => locale.value as Language)
 
   const toggleLanguage = () => {
     const newLang = locale.value === 'pl'
@@ -15,9 +27,18 @@ export const useLanguageStore = defineStore('language', () => {
     setLanguage(newLang)
   }
 
+  const initializeLanguage = () => {
+    if (langCookie.value && langCookie.value !== locale.value) {
+      locale.value = langCookie.value
+    }
+  }
+
+  initializeLanguage()
+
   return {
     currentLang,
     setLanguage,
     toggleLanguage,
+    initializeLanguage,
   }
 })
